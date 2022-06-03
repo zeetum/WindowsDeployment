e679d38ad38c1fe1adfc1126d98fdf9ee86a5b27
zeetum <timothy.blackburn@education.wa.edu.au>
2022-06-03 19:26:56 +0800

    Update GetLoginCredentials.ps1

--- JoinDomain/GetLoginCredentials.ps1
+++ JoinDomain/GetLoginCredentials.ps1
@@ -1,15 +1,22 @@
+
 function GetLocalDomainController() {
     $DHCPServer = Get-CimInstance Win32_NetworkAdapterConfiguration -Filter "DHCPEnabled=$true" | Select DHCPServer
     $DHCPServer = $DHCPServer.DHCPServer | Out-String
     $LocalDC = [System.Net.Dns]::GetHostByAddress($DHCPServer.Trim()).HostName
+
+    return "e5070s01sv001.indigo.schools.internal"
+    #return ""
 }
 
 function GetCredentials() {
+    # an exit flag for events
+    $exit = 0
+
 	Add-Type -AssemblyName System.Windows.Forms
 	Add-Type -AssemblyName System.Drawing
 	$form = New-Object System.Windows.Forms.Form
 	$form.Text = 'Enter Administrator Credentials'
-	$form.Size = New-Object System.Drawing.Size(350,300)
+	$form.Size = New-Object System.Drawing.Size(350,240)
 	$form.StartPosition = 'CenterScreen'
 
 	$usernameLabel = New-Object System.Windows.Forms.label
@@ -38,29 +45,56 @@ function GetCredentials() {
     $passwordInput.Font = New-Object System.Drawing.Font("Arial",14,[System.Drawing.FontStyle]::Regular)
     $form.Controls.Add($passwordInput)
 
-	$localDCLabel = New-Object System.Windows.Forms.label
-	$localDCLabel.Location = New-Object System.Drawing.Size(7,92)
-	$localDCLabel.width = 100
-	$localDCLabel.Font = New-Object System.Drawing.Font("Arial",14,[System.Drawing.FontStyle]::Regular)
+	$DCPrompt = New-Object System.Windows.Forms.label
+	$DCPrompt.Location = New-Object System.Drawing.Size(10,90)
+	$DCPrompt.width = 300
+    $DCPrompt.text = "Local Domain Controller:"
+	$DCPrompt.Font = New-Object System.Drawing.Font("Arial",14,[System.Drawing.FontStyle]::Regular)
+	$form.Controls.Add($DCPrompt)
+
+    $localDCLabel = New-Object System.Windows.Forms.label
+	$localDCLabel.Location = New-Object System.Drawing.Size(10,120)
+	$localDCLabel.width = 300
+	$localDCLabel.Font = New-Object System.Drawing.Font("Arial",11,[System.Drawing.FontStyle]::Regular)
 	$form.Controls.Add($localDCLabel)
 
 	$okButton = New-Object System.Windows.Forms.Button
-	$okButton.Location = New-Object System.Drawing.Point(10,120)
-	$okButton.Size = New-Object System.Drawing.Size(260,23)
-	$okButton.Text = 'Connect or Retry'
-	$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+	$okButton.Location = New-Object System.Drawing.Point(20,160)
+	$okButton.Size = New-Object System.Drawing.Size(140,30)
+    $okButton.Font = New-Object System.Drawing.Font("Arial",14,[System.Drawing.FontStyle]::Regular)
+	$okButton.Text = 'Connect'
+    $okButton.DialogResult = "OK"
 	$form.AcceptButton = $okButton
 	$form.Controls.Add($okButton)
 
+	$cancelButton = New-Object System.Windows.Forms.Button
+	$cancelButton.Location = New-Object System.Drawing.Point(170,160)
+	$cancelButton.Size = New-Object System.Drawing.Size(140,30)
+    $cancelButton.Font = New-Object System.Drawing.Font("Arial",14,[System.Drawing.FontStyle]::Regular)
+	$cancelButton.Text = 'Cancel'
+    $cancelButton.DialogResult = "Cancel"
+    $form.CancelButton = $cancelButton
+    $form.Controls.Add($cancelButton)
+
     do {
-        $localDC = GetLocalDomainController
-        $localDCLabel.Text = $localDC
-		$form.ShowDialog()
+        $DomainControllerFQDN = GetLocalDomainController
+        Write-Output ($DomainControllerFQDN -eq "")
+        $localDCLabel.Text = $DomainControllerFQDN
+        if ($DomainControllerFQDN -eq "") {
+            $okButton.Text = "Retry"
+        } else {
+            $okButton.Text = "Connect"
+        }
+        $action = $form.ShowDialog()
+        if ($action -eq "Cancel") {return 0}
+
         $username = $usernameInput.Text
         $password = $passwordInput.Text
-	} while (!$localDC)
+        
+	} while ($DomainControllerFQDN -eq "")
 
-	return @{'username' = $username; "password" = $password; "localDC" = $localDC}
+
+	return @{'username' = $username; "password" = $password; "localDC" = $DomainControllerFQDN}
 } # Site Function Choose-SiteCode
 
 GetCredentials
