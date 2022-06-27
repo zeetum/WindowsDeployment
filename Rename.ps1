9ef64e6e9f4e167a52e42636cab173dfdb09cba2
zeetum <timothy.blackburn@education.wa.edu.au>
2022-06-27 14:25:32 +0800

    Update Rename.ps1

--- Rename.ps1
+++ Rename.ps1
@@ -1,19 +1,14 @@
-#region Get the MAC Address to set the computer name
 #Get a list of Network Adapters that are Physical and connected to the PCI bus
 $NICs = Get-WmiObject -Class Win32_NetworkAdapter | Where-Object -FilterScript {$PSItem.PhysicalAdapter} | Where-Object { $PSItem.PNPDeviceID -like "PCI\*" }
 # USB Ethernet adapters and the Surface Dock Ethernet adapter have a VendorID starting with USB.
 # Therefore, using PCI should eliminate all Ethernet adapters that are not connected using a PCI connection
    
 $MACAddress = ""
-If ($NICs | Where-Object { $PSItem.NetConnectionID -eq "WiFi" })
-{   #If there is a Physical ethernet adapter, use that MAC address
+If ($NICs | Where-Object { $PSItem.NetConnectionID -eq "WiFi" }) {
     $MACAddress = @($NICs | Where-Object { $PSItem.NetConnectionID -like "WiFi" })[0].MACAddress.Replace(':','')
-}
-ElseIf ($NICs | Where-Object { $PSItem.NetConnectionID -eq "Ethernet" })
-{   #Otherwise, use the Wi-Fi MAC address if it exists
+} ElseIf ($NICs | Where-Object { $PSItem.NetConnectionID -eq "Ethernet" }) {
     $MACAddress = @($NICs | Where-Object { $PSItem.NetConnectionID -like "Ethernet" })[0].MACAddress.Replace(':','')
 }
-#endregion
 
 If ($MACAddress) {
     Rename-Computer -NewName "MS$MACAddress" -Force
