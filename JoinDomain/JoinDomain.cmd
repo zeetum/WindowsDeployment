5328a66211223da01e26767c57202a7b721aee5e
zeetum <timothy.blackburn@education.wa.edu.au>
2024-08-06 12:25:08 +0800

    Update JoinDomain.cmd

--- JoinDomain/JoinDomain.cmd
+++ JoinDomain/JoinDomain.cmd
@@ -1,11 +1,2 @@
-@echo off
-CLS
-
-:wait
-ping localhost -n 2 > nul
 echo Starting Powershell Domain Join Script...
-
-:Call DomainJoin Powershell
 powershell.exe -noprofile -executionpolicy bypass -file JoinDomain.ps1
-
-exit
